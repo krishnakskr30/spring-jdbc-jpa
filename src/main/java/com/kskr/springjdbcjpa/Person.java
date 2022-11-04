81ccc049ae769fcd8527285c041e3ee95093b908
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-11-05 00:53:35 +0530

    Add Person entity class

---
+++ src/main/java/com/kskr/springjdbcjpa/Person.java
@@ -0,0 +1,60 @@
+package com.kskr.springjdbcjpa;
+
+public class Person {
+    private int id;
+    private String name;
+    private String location;
+    private String birthDate;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String location, String birthDate) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                '}'+'\n';
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
