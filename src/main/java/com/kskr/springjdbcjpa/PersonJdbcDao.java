865203f7f4ec47a25db7a0ada9efc6ae5f13c006
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-11-05 00:54:37 +0530

    Add findall method to get all the persons

---
+++ src/main/java/com/kskr/springjdbcjpa/PersonJdbcDao.java
@@ -0,0 +1,19 @@
+package com.kskr.springjdbcjpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
