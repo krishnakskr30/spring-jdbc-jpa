daba986cbbc3c9abfc7a19256cc6b234c1a6e701
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-11-05 00:55:30 +0530

    Refactor the SpringJdbcJpaApplication to log the persons details

--- src/main/java/com/kskr/springjdbcjpa/SpringJdbcJpaApplication.java
+++ src/main/java/com/kskr/springjdbcjpa/SpringJdbcJpaApplication.java
@@ -1,13 +1,25 @@
 package com.kskr.springjdbcjpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJdbcJpaApplication {
+public class SpringJdbcJpaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcJpaApplication.class, args);
-	}
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    PersonJdbcDao personJdbcDao;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All users: {}", personJdbcDao.findAll());
+    }
 }
