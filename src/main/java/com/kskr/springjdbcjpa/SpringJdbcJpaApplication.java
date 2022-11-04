2f108117625d02cba4d6d5b6ca1dc687759e5a76
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-11-04 18:20:35 +0530

    Initial commit

---
+++ src/main/java/com/kskr/springjdbcjpa/SpringJdbcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kskr.springjdbcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kskr/springjdbcjpa/SpringJdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kskr.springjdbcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
